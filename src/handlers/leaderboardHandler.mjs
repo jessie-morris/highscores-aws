755f2f3a9d277f88c23bf137da690d6b0e7aa8e6
Jessie Morris <jmorrisosu@gmail.com>
2024-09-19 23:39:42 -0400

    Now working, main item remaining is to auto create a global secondary index on dynamo

--- src/handlers/leaderboardHandler.mjs
+++ src/handlers/leaderboardHandler.mjs
@@ -2,12 +2,9 @@ import * as db from '../db.mjs'
 import * as messages from '../messages.mjs'
 
 export const leaderboardHandler = async (event) => {
-    const params = event.body;
-
+    const params = JSON.parse(event.body);
     const userId = params.username;
     const score = params.score;
-
-    console.log(score)
 
     try {
         const data = await db.insertScore(userId, score);
